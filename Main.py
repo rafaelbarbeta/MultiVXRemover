d662cba765cc78d85e9e1861b100a21a0d673d75
Rafael Barbeta <rafa.barbeta@gmail.com>
2022-12-06 10:35:44 -0300

    Fixed paths bug in Quaratine class. Added it to terminal app

--- Main.py
+++ Main.py
@@ -2,21 +2,25 @@ import os
 import HashScan
 import RulesManagement
 import Scanner
+import Quarantine
 import yara
 
 def main():
     rmng = RulesManagement.RulesManagement(os.getcwd())
     scan = Scanner.Scanner(rmng,False)
     hashScan = HashScan.HashScan(os.getcwd())
+    quar = Quarantine.Quarantine(os.getcwd())
     print("MultiVXRemover : Wipe evil software off your computer!")
-    op = "none"
     while True:
             try :
                 print("Please choose an option below:")
                 print("1) scan <directory>")
                 print("2) addRules <directory>")
                 print("3) hashScan <directory>")
-                print("4) exit")
+                print("4) viewQuarantine")
+                print("5) restore <file>")
+                print("6) remove <file>")
+                print("7) exit")
                 operation = str(input())
                 args = list(operation.split(" "))
                 if args[0] == "scan" or args[0] == "1":
@@ -25,6 +29,8 @@ def main():
                         print("Threats Detected!")
                         for key, value in fileMatches.items():
                             print(f"{key} is {value}")
+                            quar.quarantineFile(key)
+                        print("All detected threats have been quarantined")
                     else:
                         print("No threats found")
                 elif args[0] == "addRules" or args[0] == "2":
@@ -33,13 +39,28 @@ def main():
                     filesInfected = hashScan.scanDir(args[1])
                     for file in filesInfected:
                         print(file + " : Malware")
+                        quar.quarantineFile(file)
                     totalDetected = len(filesInfected)
                     totalChecked = hashScan.getFilesChecked()
                     percentOfDetection = (totalDetected / totalChecked) * 100
                     print("Results : ")
                     print("Detected " + str(totalDetected) + " out of " + str(totalChecked) + " as malware")
                     print(str(percentOfDetection) + "%")
-                elif args[0] == "exit" or args[0] == "4":
+                    print("All detected threats have been quarantined")
+                elif args[0] == "viewQuarantine" or args[0] == "4":
+                    quarantinedFiles = quar.getQuarantinedFiles()
+                    if len(quarantinedFiles) == 0:
+                        print("No files in quarantine")
+                    else:
+                        for file in quarantinedFiles:
+                            print(file)
+                elif args[0] == "restore" or args[0] == "5":
+                    quar.restoreFile(args[1])
+                    print("File restored")
+                elif args[0] == "remove" or args[0] == "6":
+                    quar.removeFile(args[1])
+                    print("File removed")
+                elif args[0] == "exit" or args[0] == "7":
                     print("Bye")
                     return 
                 else:
@@ -49,8 +70,8 @@ def main():
             except yara.Error:
                 print("Error! MultiVXRemoverCompiled directory not found!")
                 exit()
-
-
+            except FileNotFoundError:
+                print("Error! Directory or file not found!")
 
 if __name__ == '__main__':
     main()
--- Quarantine.py
+++ Quarantine.py
@@ -10,26 +10,25 @@ class Quarantine:
         if not os.path.isdir(self.quarantineDir):
             os.mkdir(self.quarantineDir)
 
-    def quarantineFile(self,file):
+    def quarantineFile(self,filePath):
         """
-        Encode 'file' in base64 and write it to the quarantine directory along with its original location and name
-        The quarantined file will be named by its hash
-        Example of quarantined file:
-
-        WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCoK:C:/Users/username/Desktop/infected.exe
+        Encode 'file' in base64 and write it to the quarantine directory along with its original location and md5 hash
+        Original file is deleted
         """
-        filePath = os.path.join(self.cwd,file)
         with open(filePath,"rb") as target:
             encodedFile = base64.b64encode(target.read())
-        fileHash = hashlib.md5(encodedFile).hexdigest()
-        with open(os.path.join(self.quarantineDir,fileHash),"wb") as target:
+        filePathQuarantine = os.path.join(self.cwd,self.quarantineDir,os.path.basename(filePath))
+        with open(os.path.join(filePathQuarantine),"wb") as target:
             target.write(encodedFile)
-        with open(os.path.join(self.quarantineDir,fileHash),"a") as target:
-            target.write(":" + filePath + ":" + file)
+        fileHash = hashlib.md5(encodedFile).hexdigest()
+        with open(os.path.join(filePathQuarantine),"a") as target:
+            target.write(":" + filePath + ":" + fileHash)
+        os.remove(filePath)
         
     def quarantineList(self,files):
         """
         Quarantine a list of files
+        'files' is a list of file paths
         """
         for file in files:
             self.quarantineFile(file)
@@ -37,26 +36,28 @@ class Quarantine:
     def restoreFile(self,file):
         """
         Decode 'file' from base64 and write it to its original location
-        The quarantined file will be named by the third field of the quarantined file
+        Remove the quarantined file from directory
+        'file' is a filename 
         """
-        filePath = os.path.join(self.quarantineDir,file)
-        with open(filePath,"rb") as target:
+        filePathQuarantine = os.path.join(self.cwd,self.quarantineDir,file)
+        with open(filePathQuarantine,"rb") as target:
             fileFields = target.read().split(b":")
             decodedFile = base64.b64decode(fileFields[0])
             originalPath = fileFields[1].decode("utf-8")
-            originalName = fileFields[2].decode("utf-8")
-        with open(os.path.join(originalPath,originalName),"wb") as target:
+        with open(originalPath,"wb") as target:
             target.write(decodedFile)
+        os.remove(filePathQuarantine)
     
-    def destroyFile(self,file):
+    def removeFile(self,filePath):
         """
         Delete a quarantined file
+        'file' is a file path
         """
-        filePath = os.path.join(self.quarantineDir,file)
         os.remove(filePath)
     
     def getQuarantinedFiles(self):
         """
         Return a list of quarantined files
+        This list is composed of file names
         """
         return os.listdir(self.quarantineDir)
